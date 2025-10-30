95e04ad63caa0cfc614d1932e8d2e4115d6b28c5
StarshineC <jonathanresponilla@gmail.com>
2025-10-30 17:15:54 -0400

    Changes to Product card styling
    Everything is now roughly formatted to be slightly correct.
    Also changed flexbox alignment for the products container.

--- js/shop.js
+++ js/shop.js
@@ -15,21 +15,134 @@ let products = [
         img: "gem_ai.jpg",
         desc: "Description Text Here",
         page: "URL ALSO"
+    },
+    {
+        name: "Test0",
+        price: "$9999.99",
+        img: "genie_ai_2.png",
+        desc: "Description Text Here",
+        page: "URL"
+    },
+    {
+        name: "Test1",
+        price: "$0.99",
+        img: "gem_ai.jpg",
+        desc: "Description Text Here",
+        page: "URL ALSO"
+    },
+    {
+        name: "Test0",
+        price: "$9999.99",
+        img: "genie_ai_2.png",
+        desc: "Description Text Here",
+        page: "URL"
+    },
+    {
+        name: "Test1",
+        price: "$0.99",
+        img: "gem_ai.jpg",
+        desc: "Description Text Here",
+        page: "URL ALSO"
+    },
+    {
+        name: "Test0",
+        price: "$9999.99",
+        img: "genie_ai_2.png",
+        desc: "Description Text Here",
+        page: "URL"
+    },
+    {
+        name: "Test1",
+        price: "$0.99",
+        img: "gem_ai.jpg",
+        desc: "Description Text Here",
+        page: "URL ALSO"
+    },
+    {
+        name: "Test0",
+        price: "$9999.99",
+        img: "genie_ai_2.png",
+        desc: "Description Text Here",
+        page: "URL"
+    },
+    {
+        name: "Test1",
+        price: "$0.99",
+        img: "gem_ai.jpg",
+        desc: "Description Text Here",
+        page: "URL ALSO"
+    },
+    {
+        name: "Test0",
+        price: "$9999.99",
+        img: "genie_ai_2.png",
+        desc: "Description Text Here",
+        page: "URL"
+    },
+    {
+        name: "Test1",
+        price: "$0.99",
+        img: "gem_ai.jpg",
+        desc: "Description Text Here",
+        page: "URL ALSO"
+    },
+    {
+        name: "Test0",
+        price: "$9999.99",
+        img: "genie_ai_2.png",
+        desc: "Description Text Here",
+        page: "URL"
+    },
+    {
+        name: "Test1",
+        price: "$0.99",
+        img: "gem_ai.jpg",
+        desc: "Description Text Here",
+        page: "URL ALSO"
+    },
+    {
+        name: "Test0",
+        price: "$9999.99",
+        img: "genie_ai_2.png",
+        desc: "Description Text Here",
+        page: "URL"
+    },
+    {
+        name: "Test1",
+        price: "$0.99",
+        img: "gem_ai.jpg",
+        desc: "Description Text Here",
+        page: "URL ALSO"
+    },
+    {
+        name: "Test0",
+        price: "$9999.99",
+        img: "genie_ai_2.png",
+        desc: "Description Text Here",
+        page: "URL"
+    },
+    {
+        name: "Test1",
+        price: "$0.99",
+        img: "gem_ai.jpg",
+        desc: "Description Text Here",
+        page: "URL ALSO"
     }
 ]
 
 function formatCard(name, price, img, desc, page) {
     return `
-    <div style="background-image: url(${img});">
+    <div class="product-card" style="background-image: url(${img});">
+        
         <div class="product-desc">
-            <h3>${name}</h3>
-            <p>${price}</p>
-            <p>${desc}</p>
-            <div>
-                <a href="${page}>More Info</a>
-                <a>Cart</a>
+            <div class="product-title">
+                <h3>${name}</h3>
+                <p class="product-price">${price}</p>
             </div>
+            <p>${desc}</p>
+            <a href="${page}">Product Page</a>
         </div>
+        <a class="product-cart">Cart</a>
     </div>
     `
 }
