74714f6839f9b0065dd88f60691b7bc75595afd1
Lykos3D <liamwillis0@gmail.com>
2025-11-12 09:49:17 -0500

    Add HomeController tests

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $hidden = [
+        'user_type',
         'password',
         'remember_token',
     ];
--- tests/Feature/Routing/HomeControllerRoutingTest.php
+++ tests/Feature/Routing/HomeControllerRoutingTest.php
@@ -1,29 +1,104 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use function Pest\Laravel\actingAs;
+
 test("Customers should automatically be routed to the about page on login", function () {
-    // TODO
+    $customer = User::factory()->create([
+        'name' => 'John Doe',
+        'email' => 'johndoe@email.com',
+        'password' => Hash::make('password123'),
+        'user_type' => 'user'
+    ]);
+
+    $response = $this->post('/login', [
+        'email' => $customer->email,
+        'password' => 'password123',
+    ]);
+
+    $response->assertRedirect(route('about'));
+});
+
+test("Customers should automatically be routed to the about page on register", function () {
+    $response = $this->post('/register', [
+        'name' => 'John Doe',
+        'email' => 'johndoe@email.com',
+        'password' => 'Password123!',
+        'password_confirmation' => 'Password123!',
+    ]);
+
+    $response->assertRedirect(route('about'));
 });
 
 test("Administrators should automatically be routed to the admin dashboard on login", function () {
-    // TODO
+    $admin = User::factory()->create([
+        'name' => 'John Doe',
+        'email' => 'johndoe@email.com',
+        'password' => Hash::make('password123'),
+        'user_type' => 'admin'
+    ]);
+
+    $response = $this->post('/login', [
+        'email' => $admin->email,
+        'password' => 'password123',
+    ]);
+
+    $response->assertRedirect(route('admin.dashboard'));
 });
 
-test("Customers should just be rerouted to the about page if they attempt to access admin dashboard", function () {
-    // TODO
+test("Administrators should be able to access the admin dashboard", function () {
+    $admin = User::factory()->create([
+        'name' => 'John Doe',
+        'email' => 'johndoe@email.com',
+        'password' => Hash::make('password123'),
+        'user_type' => 'admin'
+    ]);
+
+    $this->actingAs($admin);
+
+    $response = $this->get('/admin/dashboard');
+
+    $response->assertStatus(200);
+});
+
+test("Customers should just be rerouted to the home page if they attempt to access admin dashboard", function () {
+    $customer = User::factory()->create([
+        'name' => 'John Doe',
+        'email' => 'johndoe@email.com',
+        'password' => Hash::make('password123'),
+        'user_type' => 'user'
+    ]);
+
+    $this->actingAs($customer);
+
+    $response = $this->get('/admin/dashboard');
+
+    $response->assertRedirect('/');
+});
+
+test("Guests should just be rerouted to the login page if they attempt to access admin dashboard", function () {
+    $response = $this->get('/admin/dashboard');
+
+    $response->assertRedirect(route('login'));
 });
 
 test("The about route should bring the user to the About page", function () {
-    // TODO
+    $response = $this->get('/about');
+    $response->assertViewIs('about');
 });
 
 test("The contact route should bring the user to the Contact page", function () {
-    // TODO
+    $response = $this->get('/contact');
+    $response->assertViewIs('contact');
 });
 
 test("The readings route should bring the user to the Readings page", function () {
-    // TODO
+    $response = $this->get('/readings');
+    $response->assertViewIs('readings');
 });
 
 test("The shop route should bring the user to the Shop page", function () {
-    // TODO
+    $response = $this->get('/shop');
+    $response->assertViewIs('shop');
 });
