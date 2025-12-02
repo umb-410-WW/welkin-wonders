0458aa34692e74e86a5c48321fab62a3b14aa0a3
Liam Willis <Liam.Willis001@umb.edu>
2025-12-02 15:55:49 -0500

    Update edit form

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -5,20 +5,24 @@
     <form style="color: white; display: flex; flex-direction: column" action="{{ route('products.update') }}"
           method="POST" enctype="multipart/form-data">
         @csrf
+        @method('PUT')
         <label for="name">Product Name</label>
-        <input type="text" name="name" id="name" class="form-control" required>
+        <input type="text" name="name" id="name" value="{{ old('name', $product->name) }}" required>
+        @error('name')
+        <div>{{ $message }}</div>
+        @enderror
 
         <label for="slug">Slug</label>
-        <input type="text" name="slug" id="slug" required>
+        <input type="text" name="slug" id="slug" value="{{ old('slug', $product->slug) }}" required>
 
         <label for="description">Description</label>
         <textarea name="description" id="description"></textarea>
 
         <label for="price">Price</label>
-        <input type="number" step="0.01" name="price" id="price" value="0" required>
+        <input type="number" step="0.01" name="price" id="price" value="{{ old('price', $product->price) }}" required>
 
         <label for="stock_quantity">Stock Quantity</label>
-        <input type="number" name="stock_quantity" id="stock_quantity" required>
+        <input type="number" name="stock_quantity" value="{{ old('stock_quantity', $product->stock_quantity) }}" required>
 
         <input type="checkbox" name="is_active" id="is_active">
         <label for="is_active">Active</label>
