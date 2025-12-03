bcfef352bc57e173e969db55ae7eabd0b89dfd01
Lykos3D <liamwillis0@gmail.com>
2025-12-03 08:54:22 -0500

    Fix CSS for Cart page

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.ww_layouts.app')
 @section('title', 'Cart')
+@section('style')
+    <link rel="stylesheet" href="{{ asset('assets/css/cart.css') }}">
+@endsection
 @section('content')
     <section id="cart-strip">
         <div id="cart-header">
@@ -8,19 +11,19 @@
         </div>
         <div id="cart-items">
             @foreach ($cart->items as $item)
-                <div>
-                    <strong>{{ $item->product->name }}</strong>
+                <div class="cart-item">
+                    <strong class="cart-item-description">{{ $item->product->name }}</strong>
                     ({{ $item->quantity }} * ${{ $item->product->price }})
 
-                    <form action="{{ route('cart.update', $item) }}" method="POST">
+                    <form class="product-counter" action="{{ route('cart.update', $item) }}" method="POST">
                         @csrf
                         <input type="number" name="quantity" value="{{ $item->quantity }}" min="1">
                         <button>Update</button>
                     </form>
 
-                    <form action="{{ route('cart.remove', $item) }}" method="POST">
+                    <form class="cart-item-remove" action="{{ route('cart.remove', $item) }}" method="POST">
                         @csrf
-                        <button>Remove</button>
+                        <button>X</button>
                     </form>
                 </div>
             @endforeach
