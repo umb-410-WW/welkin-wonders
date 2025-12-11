642e85a104f44608c129f67c5d44ecd2e0003dab
Liam Willis <Liam.Willis001@umb.edu>
2025-12-10 22:08:32 -0500

    Update Readings page with form

--- resources/views/readings.blade.php
+++ resources/views/readings.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.ww_layouts.app')
 @section('title', 'Readings')
+@section('style')
+    <link rel="stylesheet" href= {{ asset('assets/css/contact.css') }} />
+@endsection
 @section('content')
     <section>
-        <h1>This will be the Readings page!</h1>
+        <h1>Readings</h1>
+        <p>Wonder where your life is going? Get a reading!</p>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScS9oQURWyhY9uJMujg5CtWbeGzZatnzxWVav6jVcngwMqIRg/viewform?embedded=true">
+            Loading…
+        </iframe>
     </section>
 @endsection
